c1d874348d388a76dcfd5b40b785785f8d40ace5
Ahmed-Rashid200307 <ahmedrashid2003.07@gmail.com>
2025-01-29 20:28:14 +0500

    updated pdf to html

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/generate-resume', (req,res)=> {
+app.get('/generate-resume', (req,res)=> {
   console.log("req rec");
   console.log(req.query.theme);
   const resumeData = req.body;
 
   file.writeFileSync('resume.json',JSON.stringify(resumeData,null,2));
 
-  exec(`resume serve --theme ${req.query.theme}`, (error)=>{
+  exec(`resume export resume.html --theme ${req.query.theme}`, (error)=>{
     if(error){
       return res.status(500).send('Error generating resume');
     }
 
-  // res.sendFile('C:/Users/as comp/Desktop/Resume project/resume.pdf');
+  res.sendFile('C:/Users/as comp/Desktop/Resume project/resume.html');
   })
 })
 
